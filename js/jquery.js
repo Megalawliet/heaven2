82609f5fdd809f95bdea295972734f741f941316
Megalawliet <galowmere@gmail.com>
2018-02-21 15:05:27 +0100

    canvis

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    		$("#Parapa").click(function(){
+                $("#name").removeClass("parpadea");
+                $("#email").removeClass("parpadea");
+                $("#phone").removeClass("parpadea");
+                $("#message").removeClass("parpadea");
+    });		
+});
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "Formulari incomplet!";
 	return false;
    }
 	
@@ -16,11 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$to = 'noeliabarrera@iesmontsia.org'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulari de contacte:  $name";
+$email_body = "Acabes de rebre un missatge del lloc web: Heaven.\n\n"."Aquí pots veure els detalls:\n\nNom: $name\n\nEmail: $email_address\n\nTelèfon: $phone\n\nMissatge:\n$message";
+$headers = "From: noeliabarrera@iesmontsia.org\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Envía a: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
 ?>
